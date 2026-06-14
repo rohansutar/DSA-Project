54025d9c6064bf43fb7b5d187813592dca5de5f3
Rohan <sutarrohanramchandra@gmail.com>
2026-06-14 08:16:18 +0530

    Cost to remove an element

---
+++ src/main/java/org/dsa/sorting/CostRemovalElement.java
@@ -0,0 +1,24 @@
+package org.dsa.sorting;
+
+/*
+Given N elements, at every step remove an array element.
+Cost to remove an element = Sum of array of elements present in an array Find minimum cost to remove all elements.
+*/
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CostRemovalElement {
+
+    public static void main(String[] args) {
+        Integer [] arr={4,6,1};
+
+        Arrays.sort(arr, Collections.reverseOrder());
+        int ans =0;
+        for(int i=0;i< arr.length;i++){
+            ans += arr[i] * (i+1);
+            System.out.println(ans);
+        }
+        System.out.println("Ans====> "+ans);
+    }
+}
